8a7f070daf7cd430ea2443c2eb08395f1261b1c2
“xiang2002 <2014176995@qq.com>
2020-09-13 18:18:14 +0800

    origin

--- src/accuracy.py
+++ src/accuracy.py
@@ -5,7 +5,7 @@ import cv2
 def generate_matrix(gt_image, pre_image,num_class=38):
         mask = (gt_image >= 0) & (gt_image < num_class)#ground truth中所有正确(值在[0, class_num])的像素label的mask
         label = num_class * gt_image[mask].astype('int') + pre_image[mask].astype("int")
-        # np.bincount计算了从0到n**2-1这n**2个数中每个数出现的次数，返回值形状(n, n)
+        # np.bincount计算从0到n**2-1这n**2个数中每个数出现的次数，返回值形状(n, n)
         count = np.bincount(label, minlength=num_class**2)
         confusion_matrix = count.reshape(num_class, num_class)#21 * 21(for pascal)
         return confusion_matrix
